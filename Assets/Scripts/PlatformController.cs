ef8c0f9014ceaf30c6860a8818abd639e10ef7eb
Javier Rubio Gimémez <jvrr20@gmail.com>
2014-11-16 20:42:41 +0100

    Primer diseño del nivel 1

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+	public float velocidadX;
+	public float velocidadZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotacionX = Input.GetAxis("Horizontal") * velocidadX*-1;
+		float rotationZ = Input.GetAxis("Vertical") * velocidadZ*-1;
+		transform.Rotate(rotacionX*Time.deltaTime, 0, rotationZ*Time.deltaTime);
+	}
+}
