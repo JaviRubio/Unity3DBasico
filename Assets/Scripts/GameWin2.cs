c6c3105df16c82e55a30dd181d9e0c56cef85788
Javier Rubio Gimémez <jvrr20@gmail.com>
2015-01-18 12:56:40 +0100

    Aligerado el proyecto removiendo los assets innecesarios

---
+++ Assets/Scripts/GameWin2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWin2 : MonoBehaviour {
+
+	void OnTriggerEnter (Collider myTrigger) {
+		if(myTrigger.gameObject.name == "Sphere"){
+			Application.LoadLevel("Nivel3");
+		}
+	}
+}
--- Assets/Scripts/MenuFunciones.cs
+++ Assets/Scripts/MenuFunciones.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class MenuFunciones : MonoBehaviour {
 
-	public void NuevaPartida(){
+	public void IniciaNivel1(){
 		Application.LoadLevel ("Nivel1");
+	}
+
+	public void IniciaNivel2(){
+		Application.LoadLevel ("Nivel2");
+	}
+
+	public void IniciaNivel3(){
+		Application.LoadLevel ("Nivel3");
 	}
 
 	public void SalirJuego(){
